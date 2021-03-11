3c9a92677c7466d4f5ee64fec9ec4461f38f780a
Breno Farias <41799984+brenofarias@users.noreply.github.com>
2021-03-11 15:17:17 -0300

    criação do middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 
+
+import authMiddleware from './app/middlewares/authMiddleware';
+
 import UserController from './app/controllers/UserController';
 import AuthController from './app/controllers/AuthController';
 
@@ -7,5 +10,6 @@ const router = Router();
 
 router.post('/users', UserController.store);
 router.post('/auth', AuthController.authenticate);
+router.get('/users', authMiddleware, UserController.index);
 
 export default router;
